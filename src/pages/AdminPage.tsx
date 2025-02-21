73139ce065ed77da6ef6d1f03f9ac241c00dae19
Aljon Mendiola <aljon_mendiola799224@yahoo.com>
2025-02-21 13:51:33 +0800

    Enhance toast notifications

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -45,7 +45,7 @@ export default function AdminPage() {
 			{isToastVisible && (
 				<div
 					id="toast-success"
-					className="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow mx-auto"
+					className="fixed top-4 left-1/2 transform -translate-x-1/2 flex items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow z-[9999]"
 					role="alert"
 				>
 					<div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
