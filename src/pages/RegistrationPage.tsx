219dcc39ecd06e81a6c4e137ffebb04e14d370c4
Aljon Mendiola <aljon_mendiola799224@yahoo.com>
2025-05-17 10:05:43 +0800

    refactor code

--- src/pages/RegistrationPage.tsx
+++ src/pages/RegistrationPage.tsx
@@ -83,7 +83,10 @@ export default function RegistrationPage() {
 					className="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow mx-auto"
 					role="alert"
 				>
-					<div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg">
+					<div
+						className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg"
+						id="error-icon"
+					>
 						<svg
 							className="w-5 h-5"
 							aria-hidden="true"
@@ -93,7 +96,9 @@ export default function RegistrationPage() {
 						>
 							<path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z" />
 						</svg>
-						<span className="sr-only">Error icon</span>
+						<span id="error-icon" className="sr-only">
+							Error icon
+						</span>
 					</div>
 					<div className="ms-3 text-sm font-normal">{errorMessage}</div>
 					<button
